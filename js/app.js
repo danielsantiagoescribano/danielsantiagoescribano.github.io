0f6007c7864ac1b19b32c67878a3f4af657dd122
Daniel <damx@ercilla.com>
2025-10-17 16:57:33 +0200

    Proyecto

---
+++ js/app.js
@@ -0,0 +1,4 @@
+function mostrarMensaje() {
+  const mensaje = document.getElementById("mensaje");
+  mensaje.textContent = "¡Gracias por visitar mi página! Si quieres contactarme, puedes escribirme a: [tu correo electrónico]";
+}
